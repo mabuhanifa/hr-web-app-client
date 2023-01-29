dba66bb25253f81a149ec8e38dd848240dbed54e
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-29 15:13:22 +0600

    routes updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Employees from "./components/Employees";
 import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="employees" element={<Employees />} />
+      </Route>
     </Routes>
   );
 }
