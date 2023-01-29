c5e7562087ffa685000754b6e9c2eaa0587f399d
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-29 10:43:25 +0600

    react router installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   );
 }
 
