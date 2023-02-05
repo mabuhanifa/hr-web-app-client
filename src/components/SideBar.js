1535170a96dae438588f13ed0c7e967fc8f123ea
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-05 11:56:42 +0600

    icons added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,33 @@
 import React from "react";
+import { AiFillHome, AiFillIdcard } from "react-icons/ai";
+import { FcLeave } from "react-icons/fc";
+import { MdOutlineContactSupport } from "react-icons/md";
+import { RxDashboard } from "react-icons/rx";
 import { Link } from "react-router-dom";
 
 export default function SideBar() {
   return (
     <div className="bg-gray-800 text-gray-100 h-screen p-10 w-1/6 text-lg font-semibold">
       <div className="home text-xl">
-      <Link to={"/"}>
-        <p>Home</p>
-      </Link>
+        <Link to={"/"}>
+          <p>
+            <AiFillHome className="inline pb-1 text-2xl mr-1" />
+            Home
+          </p>
+        </Link>
       </div>
       <div className="flex flex-col gap-y-12 justify-center  side_bar">
         <Link to={"/profile"}>
-          <p>Profile</p>
+          <p><AiFillIdcard  className="inline pb-1 text-2xl mr-1" />Profile</p>
         </Link>
         <Link to={"/employees"}>
-          <p>Dashboard</p>
+          <p><RxDashboard  className="inline pb-1 text-2xl mr-1" />Dashboard</p>
         </Link>
         <Link to={"/leave"}>
-          <p>Leave Applications</p>
+          <p><FcLeave className="inline pb-1 text-2xl mr-1" />Leave Applications</p>
         </Link>
         <Link to={"/contact"}>
-          <p>Contact</p>
+          <p><MdOutlineContactSupport  className="inline pb-1 text-2xl mr-1" />Contact</p>
         </Link>
       </div>
     </div>
